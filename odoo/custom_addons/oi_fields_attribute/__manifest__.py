7d49559b7082e2cbbec20aa7ca2342c31dfa881a
Abhinav-navabrind <abhinav.ms@navabrindit.com>
2025-01-08 18:28:12 +0530

    Update __manifest__.py commented

--- odoo/custom_addons/oi_fields_attribute/__manifest__.py
+++ odoo/custom_addons/oi_fields_attribute/__manifest__.py
@@ -18,28 +18,28 @@
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
     'category': 'Extra Tools',
-    'version': '17.0.1.1.14',
+    'version': '18.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'web', 'mail'],
+    # 'depends': ['base', 'web', 'mail'],
 
-    # always loaded
-    'data': [
-        'security/ir.model.access.csv',
-        'view/ir_model_fields_attribute.xml',
-        'view/ir_model_fields.xml',
-        'view/action.xml',
-        'view/menu.xml',
-    ],
-    'assets': {
-        'web.assets_backend': [
-            'oi_fields_attribute/static/src/field_attributes/field_attributes.js',
-            'oi_fields_attribute/static/src/field_attributes/field_attributes.xml'
-        ],
-    },      
-    'external_dependencies' : {
+    # # always loaded
+    # 'data': [
+    #     'security/ir.model.access.csv',
+    #     'view/ir_model_fields_attribute.xml',
+    #     'view/ir_model_fields.xml',
+    #     'view/action.xml',
+    #     'view/menu.xml',
+    # ],
+    # 'assets': {
+    #     'web.assets_backend': [
+    #         'oi_fields_attribute/static/src/field_attributes/field_attributes.js',
+    #         'oi_fields_attribute/static/src/field_attributes/field_attributes.xml'
+    #     ],
+    # },      
+    # 'external_dependencies' : {
         
-    },
+    # },
     'odoo-apps' : True,
     'auto_install': True,
     'images':[
